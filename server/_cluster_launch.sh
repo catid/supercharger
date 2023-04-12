81e896c5260426f69e9f566231d68964ee468f13
catid <mrcatid@gmail.com>
2023-04-12 02:44:10 +0000

    wip

--- server/_cluster_launch.sh
+++ server/_cluster_launch.sh
@@ -17,8 +17,18 @@ pip install -r ../requirements.txt
 # Check if a port argument is provided
 if [ -z "$1" ]; then
     # Start the server on the default port
-    python server.py
+    python server.py & pid=$!
 else
     # Start the server on the specified port
-    python server.py --listen "$1"
+    python server.py --listen "$1" & pid=$!
 fi
+
+# Set up a signal handler to kill the program when the script ends
+trap ctrl_c INT
+function ctrl_c() {
+    kill $pid
+    exit
+}
+
+# Sleep forever
+sleep infinity
